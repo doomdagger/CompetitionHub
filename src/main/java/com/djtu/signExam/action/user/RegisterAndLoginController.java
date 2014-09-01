bdbd0997373a34c2dee954d869417c074d52db6d
joesec <1209202022@qq.com>
2014-09-01 17:55:35 +0800

    update all the pages,add login and register(send email to valid the account) and so on

--- src/main/java/com/djtu/signExam/action/user/RegisterAndLoginController.java
+++ src/main/java/com/djtu/signExam/action/user/RegisterAndLoginController.java
@@ -1,10 +1,11 @@
 package com.djtu.signExam.action.user;
 
 import com.djtu.signExam.dao.support.UUIDGenerator;
+import com.djtu.signExam.model.TUserAdmin;
 import com.djtu.signExam.model.TUserStudent;
 import com.djtu.signExam.service.user.RegisterAndLoginService;
-import com.djtu.signExam.service.user.impl.RegisterAndLoginServiceImpl;
 import com.djtu.signExam.util.*;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,13 +15,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.mail.MessagingException;
-import javax.mail.Session;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
-import java.util.Objects;
 
 /**
  * Created by root on 14-7-7.
@@ -104,5 +102,71 @@ public class RegisterAndLoginController {
 
 
 
-    
+    //login and set the login session
+    @RequestMapping(value = "/indexLoginStudent",method = RequestMethod.POST)
+    public @ResponseBody String indexLoginStudent(HttpServletRequest request,@RequestParam String username,@RequestParam String pwd){
+        TUserStudent student = new TUserStudent();
+        if(username.indexOf("@")<0 || username.indexOf(".com")<0){
+            //is not the email
+            //type:0:学生帐号 1：管理员帐号 2：学院帐号 3：教务帐号
+             student = (TUserStudent) registerAndLoginService.signInByNoAndReturnObj(username,pwd,0);
+        }else{
+            student = (TUserStudent) registerAndLoginService.signInByEmailAndReturnObj(username,pwd,0);
+        }
+        if(student != null){
+            //Set the login session
+            HttpSession session = request.getSession();
+            HashMap<String,Object> userSession = new HashMap<String, Object>();
+            HashMap<String,Object> userMap = new HashMap<String, Object>();
+            userMap.put(SessionConst.U_USER_TYPE,0);//用户类型
+            userMap.put(SessionConst.U_USER_NAME,student.getUUsername());//用户姓名
+            userMap.put(SessionConst.U_USER_LINK,student.getID());//用户ID
+            //put in user session
+            session.setAttribute(SessionConst.U_LOGIN,SessionConst.U_LOGIN_SUCC);//登录标志
+            session.setAttribute(SessionConst.U_USER,userMap);
+            //return json
+            JSONObject result = new JSONObject();
+            result.put("usernameSucc",student.getUUsername()==null?"":student.getUUsername());
+            result.put("usertypeSucc",0);
+            result.put("result",StringConst.AJAX_SUCCESS);
+            return result.toString();
+        }
+        JSONObject result = new JSONObject();
+        result.put("result",StringConst.AJAX_FAIL);
+        return result.toString();
+    }
+
+    //管理员后台登录
+    @RequestMapping(value = "/indexLoginAdmin")
+    public @ResponseBody String indexLoginAdmin(HttpServletRequest request,@RequestParam String username,@RequestParam String pwd){
+        TUserAdmin tUserAdmin = registerAndLoginService.signInByEmail(username,pwd);//admin login
+        if(tUserAdmin != null){
+            //Set the login session
+            HttpSession session = request.getSession();
+            HashMap<String,Object> userSession = new HashMap<String, Object>();
+            HashMap<String,Object> userMap = new HashMap<String, Object>();
+            userMap.put(SessionConst.U_USER_TYPE,0);//用户类型
+            userMap.put(SessionConst.U_USER_NAME,tUserAdmin.getUUsername());//用户姓名
+            userMap.put(SessionConst.U_USER_LINK,tUserAdmin.getID());//用户ID
+            //put in user session
+            session.setAttribute(SessionConst.U_LOGIN,SessionConst.U_LOGIN_SUCC);//登录标志
+            session.setAttribute(SessionConst.U_USER,userMap);
+            //return json
+            JSONObject result = new JSONObject();
+            result.put("usernameSucc",tUserAdmin.getUUsername()==null?"":tUserAdmin.getUUsername());
+            result.put("usertypeSucc",tUserAdmin.getAType());//账号类型
+            result.put("result",StringConst.AJAX_SUCCESS);
+            return result.toString();
+        }
+        return StringConst.AJAX_FAIL;
+    }
+
+    //注销登录
+    @RequestMapping(value = "/logout")
+    public String logout(HttpServletRequest request){
+        HttpSession httpSession = request.getSession();
+        httpSession.setAttribute(SessionConst.U_LOGIN,SessionConst.U_LOGIN_FAIL);
+        httpSession.setAttribute(SessionConst.U_USER,null);//设置用户为空
+        return "redirect:/index";
+    }
 }
